579df35fc54a547cc7166c3913af1f8a2a8470e8
Giridhar Mohareer <giripeace@gmail.com>
2024-12-11 23:42:07 -0500

    Test

---
+++ 01_Spring_App/src/main/java/in/ashokit/beans/DieselEngine.java
@@ -0,0 +1,13 @@
+package in.ashokit.beans;
+
+public class DieselEngine {
+
+	public DieselEngine() {
+		System.out.println("DieselEngine :: Constructor");
+	}
+
+	public void start() {
+		System.out.println("DieselEngine started...");
+	}
+
+}
---
+++ 01_Spring_App/src/main/java/in/ashokit/beans/PetrolEngine.java
@@ -0,0 +1,12 @@
+package in.ashokit.beans;
+
+public class PetrolEngine {
+
+	public PetrolEngine() {
+		System.out.println("PetrolEngine :: constructor");
+	}
+
+	public void start() {
+		System.out.println("PetrolEngine started...");
+	}
+}
